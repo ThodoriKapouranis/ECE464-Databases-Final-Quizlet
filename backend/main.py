9fbf16ef64611e73a22401561db5c253e222b532
Thodori Kapouranis <thodorikap@gmail.com>
2021-11-19 14:53:42 -0500

    update gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 # /venv/ is the folder generated when we installed flask.
+import pymongo
 from flask import Flask
 from mongoCredentials import MONGO_URI # Connection link
 
 app = Flask(__name__)
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client.test
+
 
 @app.route("/")
 def hello_world():
