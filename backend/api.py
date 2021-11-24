273554c0aa32f0c1fdbd7e5fbd77f774faed9d7a
Thodori Kapouranis <thodorikap@gmail.com>
2021-11-23 22:52:18 -0500

    Set up react/chakra

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,13 @@
 # /venv/ is the folder generated when we installed flask.
 import pymongo
+# from  mongodb import auths, decks, cards, users
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>";
-
+@app.route("/api/time")
+def getCurrentTime():
+    return {'time': time.time()}
 
 
